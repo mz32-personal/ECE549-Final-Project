4f2f35ab9425ad24f9fafbc2a658e55df673adbf
Asher <hanlinm2@illinois.edu>
2022-11-23 15:00:37 -0600

    loaded dataset and set up git ignore

---
+++ gesture.py
@@ -0,0 +1,31 @@
+from classifier.dataset import GestureDataset
+from classifier.preprocess import get_transform
+from omegaconf import OmegaConf, DictConfig
+import torch
+
+path_to_config = "classifier/config/default.yaml"
+conf = OmegaConf.load(path_to_config)
+train_dataset = GestureDataset(is_train=True, conf=conf, transform=get_transform())
+test_dataset = GestureDataset(is_train=False, conf=conf, transform=get_transform())
+
+collate_fn = lambda batch: tuple(zip(*batch)) # Collate func for dataloader
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=conf.train_params.train_batch_size,
+    num_workers=conf.train_params.num_workers,
+    collate_fn=collate_fn,
+    persistent_workers = True,
+    prefetch_factor=conf.train_params.prefetch_factor,
+    shuffle=True
+)
+
+test_dataloader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=conf.train_params.test_batch_size,
+    num_workers=conf.train_params.num_workers,
+    collate_fn=collate_fn,
+    persistent_workers = True,
+    prefetch_factor=conf.train_params.prefetch_factor,
+)
+
