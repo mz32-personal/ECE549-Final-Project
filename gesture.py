83a379c12e518543f04087024d23b82611d51c72
Asher <hanlinm2@illinois.edu>
2022-12-09 00:35:54 -0600

    added training and testing code, have yet to do the actual training

--- gesture.py
+++ gesture.py
@@ -2,20 +2,25 @@ from classifier.dataset import GestureDataset
 from classifier.preprocess import get_transform
 from omegaconf import OmegaConf, DictConfig
 import torch
+import cv2
+import matplotlib.pyplot as plt
+import torchvision
+from torchmetrics.functional import accuracy, f1_score, precision, recall
+from tqdm import tqdm
 
 path_to_config = "classifier/config/default.yaml"
 conf = OmegaConf.load(path_to_config)
 train_dataset = GestureDataset(is_train=True, conf=conf, transform=get_transform())
 test_dataset = GestureDataset(is_train=False, conf=conf, transform=get_transform())
 
-collate_fn = lambda batch: tuple(zip(*batch)) # Collate func for dataloader
+def collate_fn(batch): return tuple(zip(*batch)) # Collate func for dataloader
 
 train_dataloader = torch.utils.data.DataLoader(
     train_dataset,
     batch_size=conf.train_params.train_batch_size,
     num_workers=conf.train_params.num_workers,
     collate_fn=collate_fn,
-    persistent_workers = True,
+    persistent_workers = False,
     prefetch_factor=conf.train_params.prefetch_factor,
     shuffle=True
 )
@@ -25,16 +30,56 @@ test_dataloader = torch.utils.data.DataLoader(
     batch_size=conf.train_params.test_batch_size,
     num_workers=conf.train_params.num_workers,
     collate_fn=collate_fn,
-    persistent_workers = True,
+    persistent_workers = False,
     prefetch_factor=conf.train_params.prefetch_factor,
 )
 
-# TODO: Make sense of how the dataset works
+print("loading model")
+model = torchvision.models.vgg16(progress = True, num_classes = 19)
+print("finished loading model")
+params = [p for p in model.parameters() if p.requires_grad]
+num_epochs = conf.train_params.epochs
+optimizer = torch.optim.SGD(params, lr=conf.optimizer.lr, momentum=conf.optimizer.momentum, weight_decay=conf.optimizer.weight_decay)
 
-# TODO: define a neural network model
+if __name__ == '__main__':
+    # Training:
+    print("starting training")
+    model.train() # set model in training mode
+    criterion = torch.nn.CrossEntropyLoss()
+    loss_per_epoch = []
+    for epoch in tqdm(range(num_epochs), desc = "Training"):
+        for images, labels in train_dataloader:
+            # Process input tensor
+            images = torch.stack(list(image.to(conf.device) for image in images))
+            # Forward Propagation
+            output = model(images)
+            # Process ground truth labels
+            target_labels = [label['gesture'] for label in labels]
+            target_labels = torch.as_tensor(target_labels)
+            # calculate loss function
+            loss = criterion(output, target_labels)
+            loss_per_epoch.append(loss.item())
+            # Back propagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-# TODO: set up an optimizer and loss funciton (criterion)
+    # Testing
+    all_acc = []
+    with torch.no_grad():
+        model.eval() # set model in eval mode
+        for images, labels in tqdm(test_dataloader, desc= "testing"):
+            # Process input tensor
+            images = torch.stack(list(image.to(conf.device) for image in images))
+            # Forward Propagation
+            output = model(images)
+            # Process ground truth labels
+            target_labels = [label['gesture'] for label in labels]
+            target_labels = torch.as_tensor(target_labels)
+            predicted = torch.argmax(output.detach(), dim=1)
+            acc = accuracy(predicted, target_labels, num_classes=19)
+            all_acc.append(acc.item())
+    print("Accuracy:", torch.mean(all_acc))
 
-# TODO: set up training loop and back propagation (loss.backward())
-
-# TODO: test model accuracy
+    # save model
+    torch.save(model.state_dict(), "./saved_model.pth")
